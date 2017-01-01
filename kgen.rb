4b5fe1641850b24410aae37bd72392f2c2864f22
Dru Jensen <drujensen@gmail.com>
2017-01-01 09:40:26 -0800

    update the url

--- kgen.rb
+++ kgen.rb
@@ -1,7 +1,7 @@
 class Kgen < Formula
   desc "kgen is a command line tool for Kemalyst"
   homepage "http://github.com/TechMagister/kemalyst-generator"
-  url "https://github.com/TechMagister/kemalyst-generator/archive/v0.1.0.tar.gz"
+  url "https://github.com/TechMagister/kemalyst-generator/archive/v0.1.1.tar.gz"
   version "0.1.1"
   sha256 "b024cc49cfb9ac1ca32a3d73c78ec23a9159d29166bc1ae25c0fe0eabc8235c9"
   revision 1
