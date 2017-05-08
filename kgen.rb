ecc23e61e8a5dc389115b075910c22c90eff2b84
Dru Jensen <drujensen@gmail.com>
2017-05-07 17:30:55 -0700

    bump version

--- kgen.rb
+++ kgen.rb
@@ -1,9 +1,9 @@
 class Kgen < Formula
   desc "kgen is a command line tool for Kemalyst"
   homepage "http://github.com/TechMagister/kemalyst-generator"
-  url "https://github.com/TechMagister/kemalyst-generator/archive/v0.4.4.tar.gz"
-  version "0.4.4"
-  sha256 "11e502ffc1923ed2d1b53bed0914f8d03044e77341d1f0c978a9e2edafbbd577"
+  url "https://github.com/TechMagister/kemalyst-generator/archive/v0.4.5.tar.gz"
+  version "0.4.5"
+  sha256 "55019ba4b6593d84e3687e23ff22c6ba8adc52f87d6961c45f31a148855eae21"
   revision 1
   depends_on "crystal-lang"
 
