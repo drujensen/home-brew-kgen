d283951281e87a56eb11b8d340e84f76cd535c42
Dru Jensen <drujensen@gmail.com>
2017-06-22 21:30:45 -0700

    rollback to kemalyst-model

--- kgen.rb
+++ kgen.rb
@@ -1,9 +1,9 @@
 class Kgen < Formula
   desc "kgen is a command line tool for Kemalyst"
   homepage "http://github.com/kemalyst/kemalyst-generator"
-  url "https://github.com/kemalyst/kemalyst-generator/archive/v0.7.0.tar.gz"
-  version "0.7.0"
-  sha256 "b3793cbbd73437a3efd5a08660f11ff3a4c5cd7a235f9f8d0cbec243434edca2"
+  url "https://github.com/kemalyst/kemalyst-generator/archive/v0.7.1.tar.gz"
+  version "0.7.1"
+  sha256 "f6ffb36d0024f836a46c2da727403e54f57395ef9936ea04b995df72a8bca2a7"
   revision 1
   depends_on "crystal-lang"
 
